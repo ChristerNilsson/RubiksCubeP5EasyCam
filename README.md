f3648ac923af1bcb9c9f46267947b28fd83a0de7
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-01-20 02:19:38 +0100

    Fixade allvarlig bug i SWAPS. bokstäverna roterades inte i samma riktning.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # RubiksCubeP5EasyCam
+
+## Internal representation
+```
+        U T S
+        V   Z                       Orange
+        W X Y 
+  J Q P a h g j q p A H G 
+  K   O b   f k   o B   F     Green White Blue Yellow
+  L M N c d e l m n C D E    
+        y x w
+        z   v                       Red
+        s t u 
+```
+
+
+Nu kan jag ange rotationerna och har full koll på vilken av de 24 som ligger närmast nuvarande rotation  
+https://npm.runkit.com/quaternion  
+https://en.wikipedia.org/wiki/Quaternion  
+https://www.npmjs.com/package/quaternion  
+
+```
+var Quaternion = require("quaternion")
+
+var rad = Math.PI / 180
+
+const f = (a,b,c,d) => new Quaternion([a/360,b/360,c/360,d/360])
+const g = (d3) => new Quaternion(d3)
+const angle = (a,b) => 2*Math.asin(g(a.div(b).imag()).norm())/rad
+const fe = (a,b,c) => Quaternion.fromEuler(a*rad, b*rad, c*rad, 'ZXY')
+
+const x = fe(0,0,0) // 1 0 0 0 
+angle(x,fe(0,0,0)) // 0
+angle(x,fe(90,0,0)) // 89.999
+angle(x,fe(180,0,0)) // 180.000
+angle(x,fe(270,0,0)) // 90.000
+angle(x,fe(0,90,0)) // 90
+angle(x,fe(0,0,90)) // 89.999
+angle(x,fe(90,90,0)) // 120
+angle(x,fe(90,0,90)) // 120
+angle(x,fe(0,90,90)) // 120
+angle(x,fe(90,90,90)) // 180
+```
--- sketch.js
+++ sketch.js
@@ -1,28 +1,56 @@
 // TODO #########
-// vinkelkontroll 
+// vinkelkontroll
 // återställ
-// undo 
-// scramble 
+// undo
+// scramble
 // visa hjälp, även för macros
 // transparens
 
 const range = _.range
 const log = console.log
+const assert = (a,b) => {
+	if (!_.isEqual(a,b)) {
+		log('assert failed')
+		log(' ',a)
+		log(' ',b)
+		// debug.assert(a==b)
+	}
+}
+
+function adapt (q) {
+	return [q.x,q.y,q.z,q.w]
+}
+
+const rad = Math.PI / 180
+const g = (d3) => new Quaternion(d3)
+const angle = (a,b) => {
+	const aa = Quaternion({x:a[0], y:a[1], z:a[2], w:a[3]})
+	const bb = Quaternion({x:b[0], y:b[1], z:b[2], w:b[3]})
+	return 2*Math.asin(g(aa.div(bb).imag()).norm())/rad
+}
+const fe = (a,b,c) => Quaternion.fromEuler(45*a*rad, 45*b*rad, 45*c*rad, 'ZXY')
+
+const x = adapt(fe(0,0,0)) // 1 0 0 0 
+log(x)
 
 const COLORS = "#FFF #00F #FF0 #0F0 #FA5 #F00".split(' ') // W B Y G O R
 const ALPHABET = 'abcdefgh jklmnopq ABCDEFGH JKLMNOPQ STUVWXYZ stuvwxyz'
 const SWAPS = { // See README.md
 	W: 'aceg bdfh wjWN xkXO ylYP',
 	B: 'lnpj moqk euAY fvBZ gwCS',
-	Y: 'GECA HFDB nsJS otKT puLU',
-	G: 'PNLJ QOMK EyaU FzbV GscW',
-	O: 'YWUS ZXVT ajGJ hqHQ gpAP',
+	Y: 'ACEG BDFH nsJS otKT puLU',
+	G: 'JLNP KMOQ EyaU FzbV GscW',
+	O: 'SUWY TVXZ ajAJ hqHQ gpGP',
 	R: 'suwy tvxz LClc MDmd NEne'
 }
 
-const cube = _.map(range(54), (i)=> Math.floor(i/9))
+let cube 
+reset()
 const SIZE = 60
 let namn // t ex RBW
+
+//const R =[adapt(fe(3,5,6)),     adapt(fe(1,5,6)),     adapt(fe(1,-1,2)),     adapt(fe(5,7,2)),    "BWGY"]
+//const O =[adapt(fe(7,3,2)),     adapt(fe(1,3,2)),     adapt(fe(3,3,2)),     adapt(fe(5,3,2)),    "WBYG"]
 
 const W =[[-123, -70,-167, 286],[ 287, 164,  72,-122],[ 288, 167, -67, 120],[ 117,  68,-164, 290],"BOGR"]
 const R =[[-199, 149, -37, 258],[ 258,  36,-154,-196],[-161,-196, 253,  31],[  37,-254, 197,-159],"BWGY"]
@@ -35,13 +63,66 @@ const rotation = [0,0,0,0,0,0] // 0..3
 
 let easycam
 let macros = {}
-let input 
+let input
+let frozen = [0,0,0,1]
+
+function reset() {
+	cube = _.map(range(54), (i)=> Math.floor(i/9))
+}
+log(cube.slice(0,9))
+log(cube.slice(9,18))
+log(cube.slice(18,27))
+log(cube.slice(27,36))
+log(cube.slice(36,45))
+log(cube.slice(45,54))
 
 function preload() {f = loadFont('https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf')}
 
 function txt(state,i,color) {
 	fill(color)
-	text(nfs(Math.round(360*state.rotation[i]), 1, 0),25,45+40*i)
+	text(nfs((state.rotation[i]), 1, 3),25,45+40*i)
+}// *360
+
+// function dist4D(a,b) {
+// 	let summa = 0
+// 	// log(a,b)
+// 	for (const i of range(4)) {
+// 		const d = a[i] - 360*b[i]
+// 		summa += d * d
+// 	}
+// 	return Math.round(Math.sqrt(summa))
+// }
+// assert(dist4D([-120,-67,-141,284],[-130/360,-57/360,-151/360,294/360]),20)
+// assert(dist4D([-130,-57,-151,294],[-130/360,-57/360,-151/360,294/360]),0)
+
+
+function closest(rotation) {
+	if (!vectors) return [0,0],0
+	let best = [0,0]
+	let value = 999999
+	// const rotation2 = [rotation[0]/360, rotation[1]/360, rotation[2]/360, rotation[3]/360]
+	for (const i of range(6)) {
+		for (const j of range(4)) {
+			const arr = vectors[i][j]
+			const dst = angle([arr[0]/360,arr[1]/360,arr[2]/360,arr[3]/360],rotation)
+			if (dst < value) {
+				value = dst
+				best = [i,j]
+			}
+		}
+	}
+	return [best,value]
+}
+assert(closest([-130/360,-57/360,-151/360,294/360]),[[0,0],23])
+
+function makeNamn2(index,j) {
+	const four = vectors[index][4]
+	// const ri = rotation[index]
+	let res = "WRGYOB"[index] + (four+four).slice(j,j+2)
+	for (const i in range(3)) {
+		res += opposite(res[i])
+	}
+	return res
 }
 
 function hud() {
@@ -52,7 +133,20 @@ function hud() {
 	txt(state,1,"green")
 	txt(state,2,"red")
 	txt(state,3,"white")
-	if (namn) text(namn.slice(0,3),25,45+40*4)
+	if (namn) text(namn.slice(0,3),500,30)
+	//text(angle(frozen,state.rotation),25,45+40*4)
+	
+	const [[i,j],dst] = closest(state.rotation)
+	if (dst < 40) {
+	 	namn = makeNamn2(i,j)
+		// frozen = vectors[i][j]
+		// rotation[i] = j
+	} else {
+		namn = ''
+	}
+	
+	text(Math.round(dst,3),25,45+40*5)
+
 	easycam.endHUD()
 }
 
@@ -75,25 +169,29 @@ function update(index) {
 	namn = makeNamn(index)
 	state.rotation = vectors[index][rotation[index]]
 	rotation[index] = (rotation[index]+1) % 4
+	frozen = _.cloneDeep(state.rotation)
 	easycam.setState(state, 250)
 	easycam.state_reset = state
 }
 
-function btn(lbl,x,y,index) { createButton(lbl).position(x,y).mousePressed(() => update(index)) }
+function btn(lbl,x,y,index) { createButton(lbl).position(x,y).size(60).mousePressed(() => update(index)) }
 
 setup = () => {
-	createCanvas(500, 500, WEBGL)
+	createCanvas(windowWidth, windowHeight, WEBGL)
 	easycam = createEasyCam()
-	state = { distance: 1000, center  : [0, 0, 0], rotation: [0,0,0,1] }
+	log(easycam)
+
+
+	state = { distance: 1000, center: [0, 0, 0], rotation: adapt(fe(3,5,2)) }
 	easycam.setState(state, 1000)
 	easycam.state_reset = state
 
-	btn ('white', 340,510, 0)
-	btn ('red',   180,510, 1)
-	btn ('green',  20,510, 2)
-	btn ('yellow',420,510, 3)
-	btn ('orange',260,510, 4)
-	btn ('blue',  100,510, 5)
+	btn ('white', 420,40, 0)
+	btn ('red',   420,70, 1)
+	btn ('green', 500,100, 2)
+	btn ('yellow',500,40, 3)
+	btn ('orange',500,70, 4)
+	btn ('blue',  420,100, 5)
 
 svekub = `SveKub
 U' L' U L - U F U' F'
@@ -111,10 +209,10 @@ f r u R U F
 u r U L u R U l
 u R U r`
 
-	createButton('save').position(430,550).mousePressed(saveMacros) 
-	input = createElement('textarea').position(20,540).id('macros')
-	input.attribute('rows',10)
-	input.attribute('cols',50)
+	createButton('save').position(420,10).size(60).mousePressed(saveMacros) 
+	input = createElement('textarea').position(120,10).id('macros')
+	input.attribute('rows',7)
+	input.attribute('cols',35)
 	input.value(svekub)
 
 	saveMacros()
@@ -135,7 +233,6 @@ function saveMacros () {
 		.replaceAll("R2",'rr').replaceAll("L2",'ll')
 		.replaceAll("F2",'ff').replaceAll("B2",'bb')
 		.split('\n').slice(1)
-	macros = {}
 	for (i of range(lines.length)) {
 		macros[i] = lines[i]
 	}
@@ -167,15 +264,17 @@ draw = function() {
 function turn(key) { // sides
 	LETTER = key.toUpperCase()
 	const index = 'UFRDBL'.indexOf(LETTER)
-	COL = namn[index]
-	col = COL.toLowerCase() // colors
+	const COL = namn[index]
+	const col = COL.toLowerCase() // colors
 	log(col,COL,namn)
 
 	if (!(COL in SWAPS)) return
 	for (const word of SWAPS[COL].split(' ')) {
 		const [i,j,k,l] = _.map(word, (w) => ALPHABET.indexOf(w))
 		const [a,b,c,d] = LETTER != key ? [l,i,j,k] : [j,k,l,i]
-		let temp = [cube[i],cube[j],cube[k],cube[l]]
+		const temp = [cube[i],cube[j],cube[k],cube[l]]
+		log('xx',a,b,c,d,i,j,k,l)
+		// [cube[a],cube[b],cube[c],cube[d]] = [cube[i],cube[j],cube[k],cube[l]]
 		cube[a] = temp[0]
 		cube[b] = temp[1]
 		cube[c] = temp[2]
@@ -190,42 +289,41 @@ function executeMacro(key) {
 	}
 }
 
+function scramble(n=50) {
+	reset()
+	let s = ""
+	for (const i in range(n)) {
+		const ch = _.sample('UFRDBLufrdbl')
+		s+=ch
+		log({ch})
+		turn(ch)
+	}
+	log('scramble',s)
+}
+
 function keyTyped (event) {
 	if (event.target.id=='macros') return
+	if (key=='x') scramble(5)
+	if (key=='Z') reset()
 	if ("UFRDBLufrdbl".includes(key)) turn(key)
 	if (key in macros) executeMacro(key)
 }
 
-// invHash = {
-// 	0 : ["BYO","WRBYOG"],
-// 101 : ["BYO","WRBYOG"],
-// 103 : ["BOW","WRBYOG"],
-// 105 : ["BWR","WRBYOG"],
-// 107 : ["BRY","WRBYOG"],
+// let x0=0
+// let y0=0
+// let z0=0
+// function keyPressed() {
+// 	log(key)
+// 	if (key == 'ArrowDown') y0+=-1
+// 	if (key == 'ArrowUp') y0+=1
+// 	if (key == 'ArrowLeft') x0+=-1
+// 	if (key == 'ArrowRight') x0+=1
+// 	if (key == 'PageUp') z0+=-1
+// 	if (key == 'PageDown') z0+=1
+// 	log(x0,y0,z0,adapt(fe(x0,y0,z0)))
 
-// 141 : ["GOY","WRBYOG"],
-// 145 : ["GRW","WRBYOG"],
-// 143 : ["GWO","WRBYOG"],
-// 147 : ["GYR","WRBYOG"],
+// 	state.rotate = adapt(fe(x0,y0,z0))
 
-// 716 : ["OWB","WRBYOG"],
-// 736 : ["OGW","WRBYOG"],
-// 756 : ["OYG","WRBYOG"],
-// 776 : ["OBY","WRBYOG"],
-
-// 316 : ["RGY","GOYBRW"], // ok
-// 336 : ["RYB","RBWOGY"], // ok
-// 356 : ["RBW","GRWBOY"], // ok
-// 376 : ["RWG","GWOBYR"], // ok
-
-// 314 : ["WGR","WOGYRB"], // ok
-// 334 : ["WRB","WGRYBO"], // ok
-// 354 : ["WBO","WRBYOG"], // ok
-// 374 : ["WOG","WBOYGR"], // ok
-
-// 714 : ["YOB","YBRWGO"], // ok
-// 734 : ["YGO","WRBYOG"],
-// 754 : ["YRG","WRBYOG"],
-// 774 : ["YBR","WRBYOG"],
+// 	easycam.setState(state, 250)
+// 	easycam.state_reset = state
 // }
-
